10c0548a0ec5abcdccb98bbd0c8268fc4cffb968
prochoim <prochoi.m@gmail.com>
2025-04-29 21:50:50 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+const MY_DOMAIN = "trendautoposter.com"
+const MY_NOTION_PAGE = "1e45c8337ae6808cad6afde765d4d099"
+
+const GOOGLE_ANALYTICS_ID = ""
+
+const CUSTOM_SCRIPT = `
+  console.log("Loaded custom script");
+`
